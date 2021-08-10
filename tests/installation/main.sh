1042e3c67a0717a8dea16b327489c5f30f29440e
Alexander Sosedkin <monk@unboiled.info>
2021-08-10 21:11:10 +0200

    tests/installation: smoke-test vanilla-clone

--- tests/installation/main.sh
+++ tests/installation/main.sh
@@ -4,21 +4,48 @@
 
 set -ueo pipefail
 
-echo 'output check: git-better'
-[[ "$(git-better 2>&1)" == "$(git 2>&1)" ]]
-echo 'output check: git-better clone'
-[[ "$(git-better clone 2>&1)" == "$(git clone 2>&1)" ]]
-echo 'output check: git-better vanilla-clone'
-[[ "$(git-better vanilla-clone 2>&1)" == "$(git clone 2>&1)" ]]
 
-echo 'remembering outputs...'
-git_output=$(git 2>&1 || true)
-git_clone_output=$(git clone 2>&1 || true)
-echo 'setting up an alias...'
-shopt -s expand_aliases
-alias git="git-better"
+echo 'setup: temporary directory'
+	tmpdir=$(realpath $(mktemp -d))
+	trap '[[ "$tmpdir" =~ /tmp. ]]; cd -; rm -rf "$tmpdir" ' EXIT
+	cd $tmpdir
+
+echo 'setup: git repo'
+	export GIT_AUTHOR_NAME=test
+	export GIT_AUTHOR_EMAIL=test@example.org
+	export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+	export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
+	mkdir repo
+	pushd repo
+		git init
+		echo 1 > a
+		git add a
+		git commit -m 'initial commit'
+		echo 2 > a
+		git add a
+		git commit -m 'second commit'
+	popd
+	unset GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL
+	unset GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL
+
+echo 'output check: git-better'
+	[[ "$(git-better 2>&1)" == "$(git 2>&1)" ]]
+echo 'output check: git-better clone'
+	[[ "$(git-better clone 2>&1)" == "$(git clone 2>&1)" ]]
+echo 'output check: git-better vanilla-clone'
+	[[ "$(git-better vanilla-clone 2>&1)" == "$(git clone 2>&1)" ]]
+
+echo 'setup: alias'
+	shopt -s expand_aliases
+	alias git-original="$(command -v git)"
+	alias git=git-better
 
 echo 'aliased output check: git=git-better'
-[[ "$(git 2>&1)" == "$git_output" ]]
+	[[ "$(git 2>&1)" == "$(git-original 2>&1)" ]]
 echo 'aliased output check: git=git-better vanilla-clone'
-[[ "$(git vanilla-clone 2>&1)" == "$git_clone_output" ]]
+	[[ "$(git vanilla-clone 2>&1)" == "$(git-original clone 2>&1)" ]]
+
+echo 'smoke test: compare two clones'
+	git-original clone repo clone-original
+	git vanilla-clone repo clone-better
+	diff --exclude index -r clone-original clone-better
