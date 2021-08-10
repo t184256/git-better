cd457e07d07845902a881aada1d9552f89862d5c
Alexander Sosedkin <monk@unboiled.info>
2021-08-10 20:40:29 +0200

    tests/installation/main.sh: test aliasing

--- tests/installation/main.sh
+++ tests/installation/main.sh
@@ -2,9 +2,23 @@
 # SPDX-FileCopyrightText: 2021 git-better authors (see AUTHORS)
 # SPDX-License-Identifier: License: GPL-3.0-or-later
 
-set -uexo pipefail
+set -ueo pipefail
 
-echo 'Output check: git-better'
+echo 'output check: git-better'
 [[ "$(git-better 2>&1)" == "$(git 2>&1)" ]]
-echo 'Output check: git-better clone'
+echo 'output check: git-better clone'
 [[ "$(git-better clone 2>&1)" == "$(git clone 2>&1)" ]]
+echo 'output check: git-better vanilla-clone'
+[[ "$(git-better vanilla-clone 2>&1)" == "$(git clone 2>&1)" ]]
+
+echo 'remembering outputs...'
+git_output=$(git 2>&1 || true)
+git_clone_output=$(git clone 2>&1 || true)
+echo 'setting up an alias...'
+shopt -s expand_aliases
+alias git="git-better"
+
+echo 'aliased output check: git=git-better'
+[[ "$(git 2>&1)" == "$git_output" ]]
+echo 'aliased output check: git=git-better vanilla-clone'
+[[ "$(git vanilla-clone 2>&1)" == "$git_clone_output" ]]
